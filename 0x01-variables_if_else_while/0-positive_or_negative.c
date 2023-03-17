02f1410d99dd53e2e3d77434fe14d74ae9092fbf
Jihane <jihanekharachi@gmail.com>
2023-03-17 15:07:08 -0700

    Task0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,7 +17,7 @@ int main(void)
 	{
 		printf("%d is positive\n", n);
 	}
-	if else (n == 0)
+	if (else (n == 0))
 	{
 		printf("%d is zero\n", n);
 	}
@@ -26,4 +26,4 @@ int main(void)
 		printf("%d is negative\n", n);
 	}
 	return (0);
-}
+
