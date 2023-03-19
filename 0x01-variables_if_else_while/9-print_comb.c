7ed8183695634932803ecf7f3ef789fa4d1d2528
Jihane <jihanekharachi@gmail.com>
2023-03-18 19:05:21 -0700

    Task9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *main - Program that pints all possible combinations of single-digit numbers.
+ *Return: Always 0.
+ */
+
+int main(void)
+{
+	int nr;
+
+	for (nr = 0; nr <= 9; nr++)
+	{
+		putchar((nr % 10) + '0');
+		if (nr == 9)
+			continue;
+		
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
