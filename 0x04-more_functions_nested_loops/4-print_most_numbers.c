e4376d87e64181e4823f336de1dc2e092350b775
Jihane <jihanekharachi@gmail.com>
2023-03-24 07:49:12 -0700

    4. I believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-*print_most_numbers - prints the numbers from 0 to 9 without 2 and 4
+*print_most_numbers - prints the numbers 01356789.
 */
 
 void print_most_numbers(void)
