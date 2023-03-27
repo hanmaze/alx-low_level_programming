0daaa18fc694fb554b0886f37dab51b07bef0a2b
Jihane <jihanekharachi@gmail.com>
2023-03-27 07:58:00 -0700

    Task4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,7 +2,7 @@
 
 /**
 *print_rev - prints a string, in reverse
-*@s: the string to be printed in reverse
+*@s: the string to be printed
 */
 
 void print_rev(char *s)
@@ -10,9 +10,10 @@ void print_rev(char *s)
 	int i, n;
 
 	n = 0;
+
 	while (s[n] != '\0')
 		n++;
-	for (i = n - 1 ; i >= 0 ; i++)
+	for (i = n - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
