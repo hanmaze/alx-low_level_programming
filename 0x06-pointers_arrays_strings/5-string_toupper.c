d4bc53c38c86d10c5228e071fb34e98ee916f82f
Jihane <jihanekharachi@gmail.com>
2023-03-30 05:44:30 -0700

    0. strcat

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+* string_toupper - changes all lowercase letters of a string to uppercase.
+* @str: the string to the changed string
+*
+* Return: a pointer to the changed string
+*/
+
+char *string_toupper(char *)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+
+		if (str[index - 1] == ' ' ||
+		    str[index - 1] == '\t' ||
+		    str[index - 1] == '\n' ||
+		    str[index - 1] == ',' ||
+		    str[index - 1] == ';' ||
+		    str[index - 1] == '.' ||
+		    str[index - 1] == '!' ||
+		    str[index - 1] == '?' ||
+		    str[index - 1] == '"' ||
+		    str[index - 1] == '(' ||
+		    str[index - 1] == ')' ||
+		    str[index - 1] == '{' ||
+		    str[index - 1] == '}' ||
+		    index == 0)
+			str[index] -= 32;
+
+		index++;
+	}
+}
