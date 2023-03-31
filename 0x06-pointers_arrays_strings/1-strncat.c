60e10bcdf761d3f5452e40c93ffc9d2c4255088a
Jihane <jihanekharachi@gmail.com>
2023-03-31 06:54:00 -0700

    1. strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,11 +2,10 @@
 
 /**
 * _strncat - concatenates two strings.
-*@dest: input value
-*@src: input value
-*@n: input value
-*
-*Return: dest
+* @dest: input value
+* @src: input value
+* @n: input value
+* Return: dest
 */
 
 char *_strncat(char *dest, char *src, int n)
