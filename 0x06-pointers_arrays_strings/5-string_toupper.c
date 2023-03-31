fec1d72cf9723e5e6c7503e907c6bc55f239d2b5
Jihane <jihanekharachi@gmail.com>
2023-03-31 07:41:06 -0700

    5. Always look up

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,35 +2,20 @@
 
 /**
 * string_toupper - changes all lowercase letters of a string to uppercase.
-* @str: the string to the changed string
-* Return: a pointer to the changed string
+* @n: pointer
+* Return: n
 */
 
 char *string_toupper(char *)
 {
-	int index = 0;
+	int j;
 
-	while (str[index])
+	j = 0;
+	while (n[j] != '\0')
 	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
-			index++;
-
-		if (str[index - 1] == ' ' ||
-		    str[index - 1] == '\t' ||
-		    str[index - 1] == '\n' ||
-		    str[index - 1] == ',' ||
-		    str[index - 1] == ';' ||
-		    str[index - 1] == '.' ||
-		    str[index - 1] == '!' ||
-		    str[index - 1] == '?' ||
-		    str[index - 1] == '"' ||
-		    str[index - 1] == '(' ||
-		    str[index - 1] == ')' ||
-		    str[index - 1] == '{' ||
-		    str[index - 1] == '}' ||
-		    index == 0)
-			str[index] -= 32;
-
-		index++;
+		if (n[j] >= 'a' && n[j] <= 'z')
+			n[j] = n[j] - 32;
+		j++;
 	}
+	return (n);
 }
